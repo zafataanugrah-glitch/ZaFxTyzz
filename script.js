09abc89d65dccc61e050bab5e79618cb4b319f69
zafataanugrah-glitch <zafataanugrah@gmail.com>
2026-02-15 05:56:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+<script>
+window.addEventListener("load", function() {
+    const loader = document.getElementById("loader");
+
+    setTimeout(() => {
+        loader.style.opacity = "0";
+        loader.style.transition = "0.5s ease";
+
+        setTimeout(() => {
+            loader.style.display = "none";
+        }, 500);
+
+    }, 1500); // durasi loading (1.5 detik)
+});
+</script>
+
+<script>
+    const text = "";
+    let i = 0;
+    const speed = 100;
+
+    function typeEffect() {
+        if (i < text.length) {
+            document.getElementById("typing").innerHTML += text.charAt(i);
+            i++;
+            setTimeout(typeEffect, speed);
+        } else {
+            document.querySelector(".cursor").style.display = "none";
+        }
+    }
+
+    window.onload = typeEffect;
+</script>
